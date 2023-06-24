9a8f21c932c22e60a119cc8a7f35c7fb3e9c8518
Princejona <57331073+Princejona@users.noreply.github.com>
2023-06-24 21:00:43 +0800

    Update debian99.sh

--- debian99.sh
+++ debian99.sh
@@ -295,10 +295,10 @@ EOM
 
 cat <<\EOM >/etc/openvpn/script/config.sh
 #!/bin/bash
-HOST='66.45.234.10'
-USER='mttunnel_dave01'
+HOST='mysql2.blazingfast.io'
+USER='vpnngpin_dave01'
 PASS='uWCUR&94Qe~$'
-DB='mttunnel_dave01'
+DB='vpnngpin_dave01'
 EOM
 
 cat <<\EOM >/etc/openvpn/script/connect.sh
